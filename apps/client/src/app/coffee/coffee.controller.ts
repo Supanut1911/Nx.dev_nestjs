326fee3ebe7da0c7df8f96dc24934b10ff5ce7dc
supanut1911 <ynotmela@gmail.com>
2021-01-11 11:46:16 +0700

    refactor client_coffeeMoudle

---
+++ apps/client/src/app/coffee/coffee.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CoffeeDto } from '../../../../../libs/data/src/lib/coffeeDto';
+import { UpdateCoffeeDto } from '../../../../../libs/data/src/lib/coffeeUpdateDto';
+import { CoffeeService } from './coffee.service';
+
+@Controller('coffee')
+export class CoffeeController {
+    constructor(private readonly coffeeService: CoffeeService) {}
+
+    @Get()
+    getCoffees() {
+        return this.coffeeService.getCoffee()
+    }
+
+    @Post()
+    addCoffee(
+        @Body() coffeeDto: CoffeeDto
+    ) {
+        return this.coffeeService.addCoffee(coffeeDto)
+    }
+
+    @Delete('/:id')
+    deleteCoffee(
+        @Param('id') id: string
+    ) {
+        return this.coffeeService.deleteCoffee(id)
+    }
+
+    @Patch('/:id')
+    updateCoffee(
+        @Param('id') id:string,
+        @Body('name') name: string,
+        @Body('price') price: number
+    ) {
+        let payload = new UpdateCoffeeDto()
+        payload.id = id
+        payload.name = name
+        payload.price = price
+
+        return this.coffeeService.updateCoffee(payload)
+    }
+}
