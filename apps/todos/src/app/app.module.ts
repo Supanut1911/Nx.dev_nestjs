31fbb0249b6508aab0a289b10035d1895d55b4cd
supanut1911 <ynotmela@gmail.com>
2021-01-11 14:05:29 +0700

    todoModule refactored

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [TodoModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
