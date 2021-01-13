37b4531db87edc89f1633e76b7ae544e835c3533
supanut1911 <ynotmela@gmail.com>
2021-01-13 13:35:10 +0700

    refactor typeOrmConfig

--- apps/coffees/src/app/app.module.ts
+++ apps/coffees/src/app/app.module.ts
@@ -3,7 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeRepository } from './coffee.repository';
-import { typeOrmConfig } from './config/typeorm.config';
+// import { typeOrmConfig } from './config/typeorm.config';
+import { typeOrmConfig } from '../../../../libs/data/src/lib/orm.config'
 import { AuthModule } from 'libs/auth/src/lib/auth.module'
 
 @Module({
