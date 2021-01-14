3922df5c5d85a56d39dace9b4cb15d0bb9b67a2f
supanut1911 <ynotmela@gmail.com>
2021-01-14 12:03:36 +0700

    remove coffeeRepository

--- apps/coffees/src/app/coffee.repository.ts
+++
@@ -1,5 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Coffee } from "../app/coffee.entity";
-
-@EntityRepository(Coffee)
-export class CoffeeRepository extends Repository<Coffee> {}
